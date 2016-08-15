2054d20c16c44b51c6a1b8bc95cd1aad47167f9d
Peter O'Shaughnessy <peter.oshaughnessy@gmail.com>
2016-08-15 18:32:27 +0100

    Remove download attribute for now

--- build/bundle.js
+++ build/bundle.js
@@ -2846,32 +2846,13 @@ var require$$0$4 = Object.freeze({
 	function download() {
 
 	  saveContext = saveCanvas.getContext('2d');
+	  saveContext.drawImage(cameraCanvas, 0, 0);
 	  saveContext.drawImage(drawingCanvas, 0, 0);
-
-	  /*
-	  let link = document.createElement('a');
-	  link.download = 'snapwat-snapshot.png';
-	  
-	  let tempHref = 'data:image/gif;base64,R0lGODlhEAAQAMQAAORHHOVSKudfOulrSOp3WOyDZu6QdvCchPGolfO0o/XBs/fNwfjZ0frl3/zy7////wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAkAABAALAAAAAAQABAAAAVVICSOZGlCQAosJ6mu7fiyZeKqNKToQGDsM8hBADgUXoGAiqhSvp5QAnQKGIgUhwFUYLCVDFCrKUE1lBavAViFIDlTImbKC5Gm2hB0SlBCBMQiB0UjIQA7';
-	   link.href = tempHref; //saveCanvas.toDataURL('image/png'); //.replace('image/png', 'image/octet-stream');
-	  link.click();
-	  link.href = '';
-	  link.download = '';
-	  */
-
-	  /*
-	  if (!localStorage || !localStorage.setItem) {
-	    console.log('Local storage not available');
-	    return;
-	  }
-	   localStorage.setItem('snapshot', saveCanvas.toDataURL('image/png'));
-	   console.log('Stored in local storage', localStorage.getItem('snapshot'));
-	  */
 
 	  var imageBlob = saveCanvas.toDataURL('image/png');
 
 	  var link = document.createElement('a');
-	  link.download = 'snapshot.png';
+	  //link.download = 'snapshot.png';
 	  link.href = '/snapshot/' + imageBlob;
 	  link.click();
 	  link.remove();
--- src/download.js
+++ src/download.js
@@ -9,35 +9,13 @@ let saveContext = null;
 function download() {
 
   saveContext = saveCanvas.getContext('2d');
+  saveContext.drawImage(cameraCanvas, 0, 0);
   saveContext.drawImage(drawingCanvas, 0, 0);
-
-  /*
-  let link = document.createElement('a');
-  link.download = 'snapwat-snapshot.png';
-  
-  let tempHref = 'data:image/gif;base64,R0lGODlhEAAQAMQAAORHHOVSKudfOulrSOp3WOyDZu6QdvCchPGolfO0o/XBs/fNwfjZ0frl3/zy7////wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAkAABAALAAAAAAQABAAAAVVICSOZGlCQAosJ6mu7fiyZeKqNKToQGDsM8hBADgUXoGAiqhSvp5QAnQKGIgUhwFUYLCVDFCrKUE1lBavAViFIDlTImbKC5Gm2hB0SlBCBMQiB0UjIQA7';
-
-  link.href = tempHref; //saveCanvas.toDataURL('image/png'); //.replace('image/png', 'image/octet-stream');
-  link.click();
-  link.href = '';
-  link.download = '';
-  */
-
-  /*
-  if (!localStorage || !localStorage.setItem) {
-    console.log('Local storage not available');
-    return;
-  }
-
-  localStorage.setItem('snapshot', saveCanvas.toDataURL('image/png'));
-
-  console.log('Stored in local storage', localStorage.getItem('snapshot'));
-  */
 
   const imageBlob = saveCanvas.toDataURL('image/png');
 
   let link = document.createElement('a');
-  link.download = 'snapshot.png';
+  //link.download = 'snapshot.png';
   link.href = `/snapshot/${imageBlob}`;
   link.click();
   link.remove();
