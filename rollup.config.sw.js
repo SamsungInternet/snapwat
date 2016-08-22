7d38fccdc8d9b8dcb467b2860b226dd3d01cefeb
Peter O'Shaughnessy <peter.oshaughnessy@gmail.com>
2016-08-22 11:26:37 +0100

    Auto-generate sw resource cache list

---
+++ rollup.config.sw.js
@@ -0,0 +1,41 @@
+/**
+ * Rollup build for the service worker script. Thanks to Rich Harris:
+ * https://gitlab.com/Rich-Harris/rollup-cache-manifest-example
+ */
+import fs from 'fs';
+import path from 'path';
+import glob from 'glob-all';
+
+function generateCacheManifest(options) {
+  return {
+    resolveId(id) {
+      if (id === options.id) {
+        return id;
+      }
+    },
+    load(id) {
+      if (id === options.id) {
+        const files = glob.sync(options.patterns)
+          .map(file => `/${file}`)
+          .concat('/');
+          return `export default ${JSON.stringify(files)}`;
+      }
+    }
+  }
+}
+
+export default {
+  entry: 'src/sw.js',
+  plugins: [
+    // \0 is a plugin convention indicating a 'virtual module' as opposed to stuff from the filesystem
+    generateCacheManifest({
+      id: '\0cache-manifest',
+      patterns: [
+        'build/bundle.js',
+        'css/styles.css',
+        'images/**',
+        'sounds/**'
+      ]
+    })
+  ]
+};
