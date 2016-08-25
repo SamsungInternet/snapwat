96e7b1f9575e6f6a123c9d036cd6a69f9b29da28
Peter O'Shaughnessy <peter.oshaughnessy@gmail.com>
2016-08-25 16:40:39 +0100

    Move hosted files under public subdirectory

---
+++ sw.js
@@ -0,0 +1,63 @@
+const CACHE_NAME = 'cache-v1';
+
+self.addEventListener('fetch', event => {
+
+  // Clone so we can consume it more than once
+  let fetchRequest = event.request.clone();
+
+  // If we can fetch latest version, then do so
+  return fetch(fetchRequest)
+    .then(response => {
+
+      if (!response || response.status >= 300 || response.type !== 'basic') {
+        // Don't cache response if it's not within our domain or not 2xx status
+        return response;
+      }
+
+      let responseToCache = response.clone();
+
+      caches.open(CACHE_NAME)
+        .then(cache => {
+          cache.put(event.request, responseToCache);
+        });
+
+      return response;
+    })
+    .catch(err => {
+
+      // Fetch failed. Maybe we're offline. Try the cache.
+      console.log('Fetch failed, try cache', err);
+
+      event.respondWith(
+        caches.match(event.request)
+          .then(response => {
+              if (response) {
+                console.log('Cache hit', event.request);
+                return response;
+              } else {
+                console.log('Offline cache miss =(');
+              }
+            }
+          )
+      );
+
+    });
+
+});
+
+// Clear out old versions
+self.addEventListener('activate', function(event) {
+  const cacheWhitelist = [CACHE_NAME];
+
+  event.waitUntil(
+    caches.keys(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) == -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
