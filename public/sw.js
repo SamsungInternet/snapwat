6b1334410d7816b6af1adf3a7da50682774404f2
Peter O'Shaughnessy <peter.oshaughnessy@gmail.com>
2016-10-14 17:11:58 +0100

    Make sw.js ES5 as no longer building it

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'cache-v1';
+var CACHE_NAME = 'cache-v1';
 
 // The advice is to not cache too much on install.
 // This is the minimal amount needed to display the initial page.
-const PRECACHE_URLS = [
+var PRECACHE_URLS = [
   '/',
   '/index.html',
   '/css/styles.css',
@@ -13,14 +13,14 @@ const PRECACHE_URLS = [
   'https://fonts.googleapis.com/css?family=Open+Sans:400,700,300,400italic,700italic,300italic,600,600italic,800,800italic'
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', function(event) {
 
   function onInstall () {
 
     console.log('Service worker installation', CACHE_NAME);
 
     return caches.open(CACHE_NAME)
-      .then(cache => {
+      .then(function(cache) {
         console.log('Caching pre-defined assets on installation...', PRECACHE_URLS);
         return cache.addAll(PRECACHE_URLS);
       });
@@ -32,11 +32,11 @@ self.addEventListener('install', event => {
 /**
  * Cache other resources, e.g. the rest of the emojis, on fetch.
  */
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', function(event) {
 
   // If we can fetch latest version, then do so
-  const responsePromise = fetch(event.request)
-    .then(response => {
+  var responsePromise = fetch(event.request)
+    .then(function(response) {
 
       if (!response || response.status >= 300 || response.type !== 'basic') {
         // Don't cache response if it's not within our domain or not 2xx status
@@ -44,21 +44,21 @@ self.addEventListener('fetch', event => {
       }
 
       // Clone it to allow us to cache it
-      const responseToCache = response.clone();
+      var responseToCache = response.clone();
 
       caches.open(CACHE_NAME)
-        .then(cache => {
+        .then(function(cache) {
           cache.put(event.request, responseToCache);
         });
 
       return response;
     })
-    .catch(err => {
+    .catch(function(err) {
 
       console.log('Fetch failed, maybe we are offline. Try cache...', err);
 
       return caches.match(event.request)
-        .then(response => {
+        .then(function(response) {
           if (response) {
             console.log('Cache hit', event.request);
             return response;
@@ -78,12 +78,12 @@ self.addEventListener('fetch', event => {
  */
 self.addEventListener('activate', function(event) {
   
-  const cacheWhitelist = [CACHE_NAME];
+  var cacheWhitelist = [CACHE_NAME];
 
   event.waitUntil(
-    caches.keys(cacheNames => {
+    caches.keys(function(cacheNames) {
       return Promise.all(
-        cacheNames.map(cacheName => {
+        cacheNames.map(function(cacheName) {
           // If it is not a current cache, delete it
           if (cacheWhitelist.indexOf(cacheName) == -1) {
             return caches.delete(cacheName);
