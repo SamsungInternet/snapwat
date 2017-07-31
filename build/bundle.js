898e94899cd0300936afddbcc94bf877aa5e4acd
Peter O'Shaughnessy <peter.oshaughnessy@gmail.com>
2017-07-31 18:30:18 +0100

    Attempting fix for Safari WebRTC check

--- build/bundle.js
+++ build/bundle.js
@@ -48,7 +48,7 @@ function WebRTCShim () {
   if (navigator.mediaDevices === undefined || navigator.mediaDevices.getUserMedia === undefined) {
 
     // First get hold of the legacy getUserMedia, if present
-    var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+    var getUserMedia = navigator.getUserMedia;
 
     // If there is no legacy getUserMedia either, do not attempt to shim, we'll check if
     // navigator.mediaDevices.getUserMedia exists in order to display the button or not.
